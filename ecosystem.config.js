d84aa2577d764439028b3d27db3ef58eb7988977
Simon Rundell <dev@codemonkey.design>
2025-08-05 07:12:13 +0100

    deploy scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [{
+    name: 'wordcloud-api',
+    script: './app-main.js',
+    instances: 1,
+    exec_mode: 'fork',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    log_file: './logs/combined.log',
+    out_file: './logs/out.log',
+    error_file: './logs/error.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    merge_logs: true,
+    max_memory_restart: '1G',
+    restart_delay: 4000,
+    watch: false,
+    time: true
+  }]
+};
